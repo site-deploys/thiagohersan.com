fa112eba729703326464ddce2e75fdaa2a87f613
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-08-28 17:27:00 -0300

    adds infinite scrolling to list pages

---
+++ themes/tgh/source/js/lol.js
@@ -0,0 +1,13 @@
+var pList = document.getElementsByClassName("project-list-container")[0];
+var numProjects = pList.childNodes.length;
+
+function appendProjects() {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    for(var i=0; i<numProjects; i++) {
+      pList.appendChild(pList.childNodes[i].cloneNode(true));
+    }
+  }
+}
+
+window.onscroll = appendProjects;
+appendProjects();
