8314c9001e319bad8b17711db6644ed968de5e4c
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-08-29 01:08:34 -0300

    tweaks infinite scroll script to load more stuff slightly before end of page

--- themes/tgh/source/js/lol.js
+++ themes/tgh/source/js/lol.js
@@ -2,7 +2,7 @@ var pList = document.getElementsByClassName("project-list-container")[0];
 var numProjects = pList.childNodes.length;
 
 function appendProjects() {
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+  if ((window.innerHeight + window.scrollY + 150) > document.body.offsetHeight) {
     for(var i=0; i<numProjects; i++) {
       pList.appendChild(pList.childNodes[i].cloneNode(true));
     }
